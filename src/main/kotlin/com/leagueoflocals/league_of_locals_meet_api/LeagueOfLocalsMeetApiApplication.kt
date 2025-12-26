59b6e37876d287fb90600f68e3f845364e21688e
AndrewTheCoder <andrew.blackwell23@yahoo.com>
2025-12-25 19:03:42 -0500

    Initial setup

---
+++ src/main/kotlin/com/leagueoflocals/league_of_locals_meet_api/LeagueOfLocalsMeetApiApplication.kt
@@ -0,0 +1,11 @@
+package com.leagueoflocals.league_of_locals_meet_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeagueOfLocalsMeetApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LeagueOfLocalsMeetApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/leagueoflocals/league_of_locals_meet_api/LeagueOfLocalsMeetApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leagueoflocals.league_of_locals_meet_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LeagueOfLocalsMeetApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
