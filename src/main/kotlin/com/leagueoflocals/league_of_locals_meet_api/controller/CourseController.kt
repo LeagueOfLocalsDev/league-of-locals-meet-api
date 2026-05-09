b99e8587b4880a75380fb1479d0ebb125d541bac
AndrewTheCoder <andrew.blackwell23@yahoo.com>
2026-05-09 10:00:50 -0500

    all paths will start with /api/v1/meets

--- src/main/kotlin/com/leagueoflocals/league_of_locals_meet_api/controller/CourseController.kt
+++ src/main/kotlin/com/leagueoflocals/league_of_locals_meet_api/controller/CourseController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
 @RestController
-@RequestMapping("api/v1/courses")
+@RequestMapping("api/v1/meets/courses")
 class CourseController(
     private val courseService: CourseService
 ) {
